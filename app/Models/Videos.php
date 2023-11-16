13d025dca1790ada618d02e99b176e9af2f7246a
MunyinyaMaurice <munyinya13@gmail.com>
2023-11-16 22:17:38 +0200

    model of videos

--- app/Models/Videos.php
+++ app/Models/Videos.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Videos extends Model
 {
     use HasFactory;
+    protected $fillable = [
+    'self_guided_short_version',
+    'short_eng_version_360_video',
+    'short_french_version_360_video',
+    'short_kiny_version_360_video',
+    
+    'long_version_self_guided',
+    'long_eng_version_360_video',
+    'long_french_version_360_video',
+    'long_kiny_version_360_video',
+    ] ;
 }
